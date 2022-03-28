cfbc68bb73c2b840b2ee662dd5b308089b87cfd5
samryn <mehmedhesham27@gmail.com>
2022-03-28 11:02:35 +0700

    Initial commit

---
+++ app/src/main/java/www/smktelkommlg/layout/MainActivity3.java
@@ -0,0 +1,12 @@
+package www.smktelkommlg.layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity3 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+}
